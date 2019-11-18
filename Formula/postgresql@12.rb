736d3162f160614a2903fe294e42a90bdfb0a209
Russell Moffitt <russmo@coralhead.com>
2019-11-17 17:10:00 -0800

    adding postgresql@12 formula

---
+++ Formula/postgresql@12.rb
@@ -0,0 +1,124 @@
+class PostgresqlAT12 < Formula
+  desc "Relational database management system"
+  homepage "https://www.postgresql.org/"
+  version = "12.1"
+  url "https://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.bz2"
+  sha256 "a09bf3abbaf6763980d0f8acbb943b7629a8b20073de18d867aecdb7988483ed"
+
+  head do
+    url "https://git.postgresql.org/git/postgresql.git", :branch => "REL_12_STABLE"
+
+    depends_on "docbook-xsl" => :build
+  end
+
+  keg_only :versioned_formula
+
+  option "with-cassert", "Enable assertion checks (for debugging)"
+  deprecated_option "enable-cassert" => "with-cassert"
+
+  depends_on "pkg-config" => :build
+
+  depends_on "e2fsprogs"
+  depends_on "gettext"
+  depends_on "icu4c"
+  depends_on "openldap"
+  depends_on "openssl"
+  depends_on "readline"
+  depends_on "tcl-tk"
+  depends_on "llvm" => :optional
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-dtrace
+      --enable-nls
+      --with-bonjour
+      --with-gssapi
+      --with-icu
+      --with-ldap
+      --with-libxml
+      --with-libxslt
+      --with-openssl
+      --with-uuid=e2fs
+      --with-pam
+      --with-perl
+      --with-python
+      --with-tcl
+      XML2_CONFIG=:
+    ]
+
+    # Add include and library directories of dependencies, so that
+    # they can be used for compiling extensions.  Superenv does this
+    # when compiling this package, but won't record it for pg_config.
+    deps = %w[gettext icu4c openldap openssl readline tcl-tk]
+    with_includes = deps.map { |f| Formula[f].opt_include }.join(":")
+    with_libraries = deps.map { |f| Formula[f].opt_lib }.join(":")
+    args << "--with-includes=#{with_includes}"
+    args << "--with-libraries=#{with_libraries}"
+
+    args << "--enable-cassert" if build.with? "cassert"
+    args << "--with-llvm" if build.with? "llvm"
+
+    extra_version = ""
+    extra_version += "+git" if build.head?
+    extra_version += " (Homebrew petere/postgresql)"
+    args << "--with-extra-version=#{extra_version}"
+
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    system "./configure", *args
+    system "make", "install-world"
+  end
+
+  def caveats; <<~EOS
+    To use this PostgreSQL installation, do one or more of the following:
+
+    - Call all programs explicitly with #{opt_prefix}/bin/...
+    - Add #{opt_bin} to your PATH
+    - brew link -f #{name}
+    - Install the postgresql-common package
+
+    To access the man pages, do one or more of the following:
+    - Refer to them by their full path, like `man #{opt_share}/man/man1/psql.1`
+    - Add #{opt_share}/man to your MANPATH
+    - brew link -f #{name}
+
+    To migrate existing data from a previous major version of PostgreSQL run:
+      brew postgresql-upgrade-database
+    EOS
+  end
+
+  plist_options :manual => "pg_ctl -D #{HOMEBREW_PREFIX}/var/postgresql@12 start"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/postgres</string>
+        <string>-D</string>
+        <string>#{var}/#{name}</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/#{name}.log</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/#{name}.log</string>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system "#{bin}/initdb", "pgdata"
+  end
+end
